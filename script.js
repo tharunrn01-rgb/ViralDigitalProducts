51ff308eaaace358584923e1dbeb28b34f03dcd8
tharunrn01-rgb <tharunrn01@gmail.com>
2025-09-06 19:47:35 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Simple function to redirect to Easebuzz checkout
 function buyNow(link) {
-  window.location.href = "upi://pay?pa=tharunrn@ybl&pn=Tharun RN&am=100&cu=INR" target="_blank">";
+  window.location.href = link;
 }
+
 
 
 
